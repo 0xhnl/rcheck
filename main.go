61cb9eb624ea0b3ddf599e31e464bd57e249c283
Htet Naing Lin <58964280+0xhnl@users.noreply.github.com>
2024-02-15 16:18:43 +0630

    Update main.go

--- main.go
+++ main.go
@@ -1,70 +1,73 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "os/exec"
-    "strings"
+        "bufio"
+        "fmt"
+        "log"
+        "os"
+        "os/exec"
+        "strings"
 )
 
 const (
-    GreenColor = "\033[0;32m"
-    RedColor   = "\033[0;31m"
-    NoColor    = "\033[0m"
+        GreenColor = "\033[0;32m"
+        RedColor   = "\033[0;31m"
+        NoColor    = "\033[0m"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: rcheck domains.txt")
-        os.Exit(1)
-    }
-    inputFile := os.Args[1]
+        if len(os.Args) < 2 {
+                fmt.Println("Usage: rcheck domains.txt")
+                os.Exit(1)
+        }
+        inputFile := os.Args[1]
 
-    file, err := os.Open(inputFile)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+        file, err := os.Open(inputFile)
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        domain := scanner.Text()
-        checkRecords(domain)
-    }
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan() {
+                domain := scanner.Text()
+                checkRecords(domain)
+        }
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+        if err := scanner.Err(); err != nil {
+                log.Fatal(err)
+        }
 }
 
 func checkRecords(domain string) {
-    spfCmd := exec.Command("dig", "+short", "TXT", domain)
-    spfOutput, err := spfCmd.Output()
-    if err != nil {
-        log.Fatalf("error running dig command for SPF: %s", err)
-    }
-    spf := string(spfOutput)
+        spfCmd := exec.Command("dig", "+short", "TXT", domain)
+        spfOutput, err := spfCmd.Output()
+        if err != nil {
+                log.Fatalf("error running dig command for SPF: %s", err)
+        }
+        spf := string(spfOutput)
 
-    dmarcCmd := exec.Command("dig", "+short", "TXT", "_dmarc."+domain)
-    dmarcOutput, err := dmarcCmd.Output()
-    if err != nil {
-        log.Fatalf("error running dig command for DMARC: %s", err)
-    }
-    dmarc := string(dmarcOutput)
+        dmarcCmd := exec.Command("dig", "+short", "TXT", "_dmarc."+domain)
+        dmarcOutput, err := dmarcCmd.Output()
+        if err != nil {
+                log.Fatalf("error running dig command for DMARC: %s", err)
+        }
+        dmarc := string(dmarcOutput)
 
-    if strings.Contains(spf, "v=spf") {
-        fmt.Printf("%sSPF record found for %s: %s%s\n", RedColor, domain, spf, NoColor)
-    } else {
-        fmt.Printf("%sSPF record not found for %s%s\n", GreenColor, domain, NoColor)
-    }
+        spfFound := strings.Contains(spf, "v=spf")
+        dmarcFound := strings.Contains(dmarc, "v=DMARC")
 
-    if strings.Contains(dmarc, "v=DMARC") {
-        fmt.Printf("%sDMARC record found for %s: %s%s\n", RedColor, domain, dmarc, NoColor)
-    } else {
-        fmt.Printf("%sDMARC record not found for %s%s\n", GreenColor, domain, NoColor)
-    }
+        if spfFound {
+                fmt.Printf("%sFound SPF record%s for %s\n", RedColor, NoColor, domain)
+        } else {
+                fmt.Printf("%sNot Found SPF record%s for %s\n", GreenColor, NoColor, domain)
+        }
 
-    fmt.Println("--------------------------")
+        if dmarcFound {
+                fmt.Printf("%sFound DMARC record%s for %s\n", RedColor, NoColor, domain)
+        } else {
+                fmt.Printf("%sNot Found DMARC record%s for %s\n", GreenColor, NoColor, domain)
+        }
+
+        fmt.Println("--------------------------")
 }
